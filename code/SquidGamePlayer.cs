909e7beb783dacf70f708ba4a5e01a5ac6fb82bf
Doodesch <30825893+DooDesch@users.noreply.github.com>
2021-10-16 19:57:04 +0200

    Add player ragdolling

--- code/SquidGamePlayer.cs
+++ code/SquidGamePlayer.cs
@@ -1,5 +1,4 @@
 ﻿using Sandbox;
-using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -7,19 +6,22 @@ namespace SquidGame
 {
 	public partial class SquidGamePlayer : Player
 	{
-		[Net] public List<GameTimer> GameTimers { get; set; } = new List<GameTimer>();
+		private DamageInfo lastDamage;
 
+		[Net] public List<GameTimer> GameTimers { get; set; } = new List<GameTimer>();
 		[Net] public AbstractGameMode CurrentGameMode { get; set; } = new NullGameMode();
 		[Net] public AbstractGameModeClient CurrentGameModeClient { get; set; } = new NullGameModeClient();
+		[Net, Predicted] public ICamera MainCamera { get; set; }
+
+		public ICamera LastCamera { get; set; }
+
 		/// <summary>
 		/// The clothing container is what dresses the citizen
 		/// </summary>
 		public Clothing.Container Clothing = new();
 
 		public bool CanMove { get; set; } = true;
-
 		public bool CanRespawn { get; set; } = true;
-
 		public bool CanSprint { get; set; } = true;
 
 		/// <summary>
@@ -30,25 +32,18 @@ namespace SquidGame
 		/// <summary>
 		/// Initialize using this client
 		/// </summary>
-		public SquidGamePlayer( Client cl )
+		public SquidGamePlayer( Client cl ) : this()
 		{
 			// Load clothing from client data
 			Clothing.LoadFromClient( cl );
-
-			// foreach ( GameTimer gameTimer in Entity.All.OfType<GameTimer>() )
-			// {
-			// 	GameTimers.Add( gameTimer );
-			// }
 		}
 
-		[Event.Entity.PostSpawn]
-		private void AddGameTimers()
+		public override void Spawn()
 		{
-			Log.Warning( "SquidGamePlayer::AddGameTimers" );
-			foreach ( GameTimer gameTimer in All.OfType<GameTimer>() )
-			{
-				GameTimers.Add( gameTimer );
-			}
+			MainCamera = new ThirdPersonCamera();
+			LastCamera = MainCamera;
+
+			base.Spawn();
 		}
 
 		public override void Respawn()
@@ -57,20 +52,11 @@ namespace SquidGame
 
 			SetModel( "models/citizen/citizen.vmdl" );
 
-			//
-			// Use WalkController for movement (you can make your own PlayerController for 100% control)
-			//
 			Controller = new SquidGameWalkController( this );
-
-			//
-			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
-			//
 			Animator = new StandardPlayerAnimator();
 
-			//
-			// Use ThirdPersonCamera (you can make your own Camera for 100% control)
-			//
-			Camera = new ThirdPersonCamera();
+			MainCamera = LastCamera;
+			Camera = MainCamera;
 
 			EnableAllCollisions = true;
 			EnableDrawing = true;
@@ -79,7 +65,49 @@ namespace SquidGame
 
 			Clothing.DressEntity( this );
 
+			// Inventory.Add( new Fists() );
+
 			base.Respawn();
+		}
+
+		public override void OnKilled()
+		{
+			base.OnKilled();
+
+			BecomeRagdollOnClient( Velocity, lastDamage.Flags, lastDamage.Position, lastDamage.Force, GetHitboxBone( lastDamage.HitboxIndex ) );
+			LastCamera = MainCamera;
+			MainCamera = new SpectateRagdollCamera();
+			Camera = MainCamera;
+			Controller = null;
+
+			EnableAllCollisions = false;
+			EnableDrawing = false;
+		}
+
+		public override void TakeDamage( DamageInfo info )
+		{
+			if ( GetHitboxGroup( info.HitboxIndex ) == 1 )
+			{
+				info.Damage *= 10.0f;
+			}
+
+			lastDamage = info;
+
+			TookDamage( lastDamage.Flags, lastDamage.Position, lastDamage.Force );
+
+			base.TakeDamage( info );
+		}
+
+		[ClientRpc]
+		public void TookDamage( DamageFlags damageFlags, Vector3 forcePos, Vector3 force )
+		{
+		}
+
+		public ICamera GetActiveCamera()
+		{
+			// if ( VehicleCamera != null ) return VehicleCamera;
+
+			return MainCamera;
 		}
 
 		/// <summary>
@@ -94,18 +122,29 @@ namespace SquidGame
 				player.CurrentGameModeClient.IsMoving = player.Velocity.Length > 0;
 			}
 
+			if ( LifeState != LifeState.Alive )
+				return;
+
+			// TickPlayerUse();
 			//
 			// If you have active children (like a weapon etc) you should call this to 
 			// simulate those too.
 			//
 			SimulateActiveChild( cl, ActiveChild );
-		}
 
-		public override void OnKilled()
-		{
-			base.OnKilled();
+			if ( Input.Pressed( InputButton.View ) )
+			{
+				if ( MainCamera is not FirstPersonCamera )
+				{
+					MainCamera = new FirstPersonCamera();
+				}
+				else
+				{
+					MainCamera = new ThirdPersonCamera();
+				}
+			}
 
-			EnableDrawing = false;
+			Camera = GetActiveCamera();
 		}
 
 		public virtual void UpdateGameTimers( int time )
@@ -113,16 +152,105 @@ namespace SquidGame
 			Log.Warning( "SquidGamePlayer::UpdateGameTimers" );
 			Log.Info( "GameTimers : " + GameTimers.Count );
 
-
 			foreach ( GameTimer gameTimer in All.OfType<GameTimer>() )
 			{
 				gameTimer.UpdateTimer( time );
 			}
 
+			// TODO : The original idea was to save the GameTimers initially in an own list
+
 			// foreach ( GameTimer gameTimer in GameTimers )
 			// {
 			// 	gameTimer.UpdateTimer( time );
 			// }
+		}
+
+		[Event.Entity.PostSpawn]
+		public void AddGameTimers()
+		{
+			Log.Warning( "SquidGamePlayer::AddGameTimers" );
+			foreach ( GameTimer gameTimer in All.OfType<GameTimer>() )
+			{
+				GameTimers.Add( gameTimer );
+			}
+		}
+
+		[ClientRpc]
+		private void BecomeRagdollOnClient( Vector3 velocity, DamageFlags damageFlags, Vector3 forcePos, Vector3 force, int bone )
+		{
+			var ent = new ModelEntity
+			{
+				Position = Position,
+				Rotation = Rotation,
+				Scale = Scale,
+				MoveType = MoveType.Physics,
+				UsePhysicsCollision = true,
+				EnableAllCollisions = true,
+				CollisionGroup = CollisionGroup.Debris
+			};
+			ent.SetModel( GetModelName() );
+			ent.CopyBonesFrom( this );
+			ent.CopyBodyGroups( this );
+			ent.CopyMaterialGroup( this );
+			ent.TakeDecalsFrom( this );
+			ent.EnableHitboxes = true;
+			ent.EnableAllCollisions = true;
+			ent.SurroundingBoundsMode = SurroundingBoundsType.Physics;
+			ent.RenderColor = RenderColor;
+			ent.PhysicsGroup.Velocity = velocity;
+
+			if ( Local.Pawn == this )
+			{
+				//ent.EnableDrawing = false; wtf
+			}
+
+			ent.SetInteractsAs( CollisionLayer.Debris );
+			ent.SetInteractsWith( CollisionLayer.WORLD_GEOMETRY );
+			ent.SetInteractsExclude( CollisionLayer.Player | CollisionLayer.Debris );
+
+			foreach ( var child in Children )
+			{
+				if ( !child.Tags.Has( "clothes" ) ) continue;
+				if ( child is not ModelEntity e ) continue;
+
+				var model = e.GetModelName();
+
+				var clothing = new ModelEntity();
+				clothing.SetModel( model );
+				clothing.SetParent( ent, true );
+				clothing.RenderColor = e.RenderColor;
+				clothing.CopyBodyGroups( e );
+				clothing.CopyMaterialGroup( e );
+			}
+
+			if ( damageFlags.HasFlag( DamageFlags.Bullet ) ||
+				 damageFlags.HasFlag( DamageFlags.PhysicsImpact ) )
+			{
+				PhysicsBody body = bone > 0 ? ent.GetBonePhysicsBody( bone ) : null;
+
+				if ( body != null )
+				{
+					body.ApplyImpulseAt( forcePos, force * body.Mass );
+				}
+				else
+				{
+					ent.PhysicsGroup.ApplyImpulse( force );
+				}
+			}
+
+			if ( damageFlags.HasFlag( DamageFlags.Blast ) )
+			{
+				if ( ent.PhysicsGroup != null )
+				{
+					ent.PhysicsGroup.AddVelocity( (Position - (forcePos + Vector3.Down * 100.0f)).Normal * (force.Length * 0.2f) );
+					var angularDir = (Rotation.FromYaw( 90 ) * force.WithZ( 0 ).Normal).Normal;
+					ent.PhysicsGroup.AddAngularVelocity( angularDir * (force.Length * 0.02f) );
+				}
+			}
+
+			Corpse = ent;
+
+			ent.DeleteAsync( 10.0f );
 		}
 	}
 }
